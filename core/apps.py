326141ba5a40ca6e6d8ba423c8765502a31709db
davidsn27 <andersonfernandes021@gmail.com>
2025-12-02 16:13:11 -0500

    Update apps.py

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,10 @@
+# core/apps.py
 from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        # <<<< ESTA LÍNEA CONECTA EL ARCHIVO signals.py AL INICIAR DJANGO
+        import core.signals 
